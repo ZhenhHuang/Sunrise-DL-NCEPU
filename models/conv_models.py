acfc9796a8482088c91529388df2d76f2fbba451
H-Joker <983117365@qq.com>
2022-08-28 21:54:39 +0800

    Update

--- models/conv_models.py
+++ models/conv_models.py
@@ -1,4 +1,3 @@
-from xml.etree.ElementInclude import include
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -40,7 +39,7 @@ class ResNetBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride):
         super(ResNetBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.norm1 = nn.BatchNorm2d(out_channels)
         self.norm2 = nn.BatchNorm2d(out_channels)
         if stride > 1:
