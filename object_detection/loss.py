09844c6febb50167db3f8315ca3460d36ec70149
H-Joker <983117365@qq.com>
2022-10-03 17:27:22 +0800

    YOLOLoss

--- object_detection/loss.py
+++ object_detection/loss.py
@@ -18,20 +18,56 @@ class YOLOLoss(nn.Module):
         :param preds: [N, S, S, 5 * B + num_classes]
         :param trues: [N, S, S, 5 * B + num_classes]
         """
+        N = preds.shape[0]
         mask = (trues[:, :, :, 4] == 1)  # N, S, S
-        pred_obj = preds[mask]     # pred boxes contain object, num_obj, 5 * B + num_classes
-        box_pred = pred_obj[:, :self.B * 5].reshape(-1, 5)
+        loss_noobj = self._loss_noobj(preds, trues, ~mask)  # loss for no obj
+
+        pred_obj = preds[mask]     # pred boxes contain object, [num_obj, 5 * B + num_classes]
+        box_pred = pred_obj[:, :self.B * 5].reshape(-1, 5)  # [num_obj_B, 5]
         cls_pred = pred_obj[:, self.B * 5:]
 
-        true_obj = trues[mask]     # true boxes contain object, num_obj, 5 * B + num_classes
+        true_obj = trues[mask]     # true boxes contain object, [num_obj, 5 * B + num_classes]
         box_true = true_obj[:, :self.B * 5].reshape(-1, 5)
         cls_true = true_obj[:, self.B * 5:]
 
-        pred_noobj = preds[~mask]
-        true_noobj = trues[~mask]
+        """compute loss for boxes with object"""
+        response_mask = torch.zeros(box_pred.shape[0], dtype=bool).to(pred_obj.device)
+        target_iou = torch.zeros(box_true.shape[0]).to(true_obj.device)
+        for i in range(0, true_obj.shape[0], self.B):
+            pred = pred_obj[i, i + self.B]  # [B, 5]
+            pred_corner = torch.zeros(pred.shape[0], 4).to(pred_obj.device)     # [B, 4]
+            pred_corner[:, :2] = pred[:, :2] - 0.5 * pred[:, 2:4]
+            pred_corner[:, 2:] = pred[:, :2] + 0.5 * pred[:, 2:4]
 
+            true = true_obj[i].unsqueeze(0)  # [1, 5]
+            true_corner = torch.zeros(true.shape[0], 4).to(true_obj.device)  # [1, 4]
+            true_corner[:, :2] = true[:, :2] - 0.5 * true[:, 2:4]
+            true_corner[:, 2:] = true[:, :2] + 0.5 * true[:, 2:4]
+
+            iou = calc_IoU(pred_corner, true_corner)    # [B, 1]
+            max_iou, max_idx = torch.max(iou, dim=0)
+            response_mask[i+max_idx] = True
+            target_iou[i+max_idx] = max_iou
+
+        box_pred_response = box_pred[response_mask].reshape(-1, 5)
+        box_true_response = box_true[response_mask].reshape(-1, 5)
+        loss_xy = F.mse_loss(box_pred_response[:, :2], box_true_response[:, :2], reduction='sum')
+        loss_wh = F.mse_loss(box_pred_response[:, 2:].sqrt(), box_true_response[:, 2:].sqrt(), reduction='sum')
+        loss_obj = F.mse_loss(box_pred_response[:, 4], target_iou, reduction='sum')
+        loss_class = F.mse_loss(cls_pred, cls_true, reduction='sum')
+
+        loss = self.w_coord * (loss_xy + loss_wh) + loss_obj + self.w_noobj * loss_noobj + loss_class
+        loss = loss / N
+        return loss
+
+    def _loss_noobj(self, preds, trues, mask):
+        pred_noobj = preds[mask]
+        true_noobj = trues[mask]
         # compute loss of the grids without object
-        loss_noobj = F.mse_loss(pred_noobj[:, [b * 4 for b in range(self.B)]])
+        loss_noobj = F.mse_loss(pred_noobj[:, [4 + b * 5 for b in range(self.B)]],
+                                true_noobj[:, [4 + b * 5 for b in range(self.B)]], reduction='sum')
+        return loss_noobj
+
 
 
 
--- object_detection/utils.py
+++ object_detection/utils.py
@@ -3,31 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def calc_IoU(bbox1, bbox2, format='corner'):
-    """
-    size: [B, 4]
-    :param format: 'corner' or 'center'. (x1, y1, x2, y2) or (x, y, w, h)
-    :return: IoU value
-    """
-    def func():
-        if format == 'corner':
-            x1, y1, x2, y2 = bbox1
-            m1, n1, m2, n2 = bbox2
-        else:
-            x1, y1, x2, y2 = center2corner(bbox1)
-            m1, n1, m2, n2 = center2corner(bbox2)
-
-        left = max(m1, x1)
-        right = min(m2, x2)
-        top = max(n1, y1)
-        bottom = min(n2, y2)
-        area1 = (x2 - x1) * (y2 - y1)
-        area2 = (m2 - m1) * (n2 - n1)
-        intersection = (right - left) * (bottom - top)
-        return intersection / (area1 + area2 - intersection)
-    areas = []
-
-    return
+def calc_IoU(bbox1: torch.Tensor, bbox2: torch.Tensor):
+    """shape: [N, 4], [M, 4] -> [N, M]"""
+    left = max(m1, x1)
+    right = min(m2, x2)
+    top = max(n1, y1)
+    bottom = min(n2, y2)
+    area1 = (x2 - x1) * (y2 - y1)
+    area2 = (m2 - m1) * (n2 - n1)
+    intersection = (right - left) * (bottom - top)
+    return intersection / (area1 + area2 - intersection)
 
 
 def center2corner(bbox):
@@ -59,5 +44,3 @@ def target_encode(target, S, B, num_classes, H, W):
     return target_tensor
 
 
-if __name__ == '__main__':
-    print(calc_IoU([2, 2, 4, 4], [3, 3, 5, 5]))
