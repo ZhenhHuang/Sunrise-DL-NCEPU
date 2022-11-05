55bbbd21ff6f8e408842973c7820844b25eaf0cd
H-Joker <983117365@qq.com>
2022-11-05 23:24:31 +0800

    Update tools.py

--- image_classification/tools.py
+++ image_classification/tools.py
@@ -41,11 +41,6 @@ class EarlyStopping:
 def adjust_learning_rate(optimizer, epoch, args):
     if args.lradj == 'type1':
         lr_adjust = {epoch: args.learning_rate * (0.5 ** (epoch // 1))}
-    elif args.lradj == 'type2':
-        lr_adjust = {
-            2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
-            10: 5e-7, 15: 1e-7, 20: 5e-8
-        }
     else:
         raise NotImplementedError
     if epoch in lr_adjust.keys():
